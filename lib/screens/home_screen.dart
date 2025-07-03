d20acd4df6c2cda33ccf2ca0ff0eee231e67c6e6
Hitesh Agarwal <>
2025-07-03 12:33:46 +0530

    Structure

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import '../utils/constants.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
