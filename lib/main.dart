d78a35f1e9435c41dde3a883219492a30f55d97d
Hitesh Agarwal <>
2025-07-03 12:33:33 +0530

    Structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:demo_messenger/screens/home_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'firebase_options.dart';
 
@@ -8,11 +10,9 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   // Initialize Firebase
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  runApp(const MyApp());
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -22,13 +22,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const Placeholder()
+      home: const HomeScreen(
+
+      ),
     );
   }
 }
-
-
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../utils/constants.dart';
+
+class HomeScreen extends ConsumerWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Icon(Icons.message), // Smaller icon
+            SizedBox(width: 2), // Smaller spacing
+            Text(
+              "E-Chat", // Smaller text
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.search, size: 32)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.add, size: 32)),
+        ],
+      ),
+      body: Column(mainAxisSize: MainAxisSize.max),
+      bottomNavigationBar: Card(
+        elevation: 12,
+        child: SizedBox(
+          width: double.infinity,
+          height: 100,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              _navItems(
+                width: width,
+                icon: Icons.chat,
+                color: Colors.transparent,
+                text: "Chat",
+              ),
+              _navItems(
+                width: width,
+                icon: Icons.chat,
+                color: Colors.transparent,
+                text: "Chat",
+              ),
+              _navItems(
+                width: width,
+                icon: Icons.chat,
+                color: Colors.transparent,
+                text: "Chat",
+              ),
+              _navItems(
+                width: width,
+                icon: Icons.chat,
+                color: Colors.blue,
+                text: "Chat",
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Widget _navItems({
+  required IconData icon,
+  required Color color,
+  required String text,
+  required double width,
+}) {
+  return Container(
+    decoration: BoxDecoration(
+      color: color,
+      borderRadius: BorderRadius.circular(12),
+    ),
+    height: 60,
+    width: width / 8,
+    child: Column(
+      children: [
+        SizedBox(height: 8),
+        Icon(
+          icon,
+          color: color != Colors.transparent ? Colors.white : Colors.black,
+        ),
+        SizedBox(height: 8),
+        Expanded(
+          child: Text(
+            text,
+            style: TextStyle(
+              fontSize: 10,
+              color: color != Colors.transparent ? Colors.white : Colors.black,
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
