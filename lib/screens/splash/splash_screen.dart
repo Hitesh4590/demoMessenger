6f014ae2b5b8d95aaba6e5198ce847b947f7efe8
akash singh <akash.singh@injala.com>
2025-07-07 11:28:30 +0530

    [fix] Remove the unused dependencies.

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'package:demo_messenger/screens/profile/profile_screen.dart';
 import 'package:flutter/material.dart';
-import '../registration/registration_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
